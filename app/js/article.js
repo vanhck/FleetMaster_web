fd1fedcdf1dad5bc1124736f5658dac13033f517
m-wessendorf <max.wessendorf@t-online.de>
2017-06-24 14:01:46 +0200

    added article activity

--- app/js/article.js
+++ app/js/article.js
@@ -7,6 +7,15 @@ function getArticleliste() {
   });
 }
 
+// returns activitylist
+function getActivityliste(id) {
+  $.get( "http://martinshare.com/api/van.php/historyware/" + id, function( data ) {
+    activitylist = JSON.parse(data);
+    //return object
+    setActivity(activitylist);
+  });
+}
+
 
 function setData(warenliste) {
   var table = document.getElementById("articletable");
@@ -17,11 +26,11 @@ function setData(warenliste) {
     $('.articleRow').remove();
   }
 
-
   for (var i = 0; i < warenliste.length; i++) {
 
     // Create an empty <tr> element and add it to the 1st position of the table:
     var row = table.insertRow(1);
+
     if (warenliste[i]["available"] == "true") {
       row.className = "articleRow success";
     } else if (warenliste[i]["available"] == "false") {
@@ -36,13 +45,35 @@ function setData(warenliste) {
     var cell2 = row.insertCell(1);
 
     // Add some text to the new cells:
-    cell1.innerHTML = warenliste[i]["id"];
+    var id = warenliste[i]["id"];
+    cell1.innerHTML = '<a onclick="getActivityliste(' + id + ')">' + id + '</a>';
     cell2.innerHTML = warenliste[i]["name"];
 
   }
 
 
 }
+
+
+function setActivity(activitylist) {
+  var table = "<table class='table table-striped table-bordered table-hover'>";
+  for (var i = 0; i < activitylist.length; i++) {
+    //table += "<td>" + activitylist[i]["einchecktyp"] + "</td>";
+    if (activitylist[i]["einchecktyp"] == "in") {
+      table += "<tr class='info'>";
+      table += "<td><i class='fa fa-long-arrow-right fa-fw'></i></td>";
+    } else if (activitylist[i]["einchecktyp"] == "out") {
+      table += "<tr class='warning'>";
+      table += "<td><i class='fa fa-long-arrow-left fa-fw'></i></td>";
+    }
+    table += "<td>" + activitylist[i]["name"] + "</td>";
+    table += "<td>" + activitylist[i]["timestamp"] + "</td>";
+    table += "</tr>";
+  }
+  table += "</table>";
+  document.getElementById("activity-body").innerHTML = table;
+}
+
 
 function addArticle() {
   var name = document.getElementById("articleName").value;
